6b14f3cf1ee7c2907bef044a0f73e85a4d2348ce
jiangzhh <akynazh@gmail.com>
2024-05-24 18:14:09 +0800

    DOCKER OPTIMIZE

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.10
 
+ENV TZ Asia/Shanghai
+
 WORKDIR /app
 
 COPY . .
--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import re
 import string
 import random
 import jvav
+import yaml
 import asyncio
 import threading
 import langdetect
@@ -12,11 +13,76 @@ import telebot
 from pyrogram import Client
 from telebot import apihelper, types
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from logger import Logger
 from config import BotConfig
 from database import BotFileDb, BotCacheDb
 from requests import get
 from requests.compat import quote
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+class Logger:
+
+    def __init__(self, path_log_file: str, log_level=logging.INFO):
+        self.logger = logging.getLogger()
+        formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        r_file_handler = RotatingFileHandler(
+            path_log_file, maxBytes=1024 * 1024 * 16, backupCount=1
+        )
+        r_file_handler.setFormatter(formatter)
+        self.logger.addHandler(r_file_handler)
+        self.logger.addHandler(stream_handler)
+        self.logger.setLevel(log_level)
+
+class BotConfig:
+    def __init__(self, path_config_file: str):
+        with open(path_config_file, "r", encoding="utf8") as f:
+            config = yaml.safe_load(f)
+        self.tg_chat_id = str(config["tg_chat_id"]) if config["tg_chat_id"] else ""
+        self.tg_bot_token = (
+            str(config["tg_bot_token"]) if config["tg_bot_token"] else ""
+        )
+        self.use_proxy = str(config["use_proxy"]) if config["use_proxy"] else "0"
+        self.use_proxy_dmm = (
+            str(config["use_proxy_dmm"]) if config["use_proxy_dmm"] else "0"
+        )
+        self.proxy_addr = str(config["proxy_addr"]) if config["proxy_addr"] else ""
+        self.use_pikpak = str(config["use_pikpak"]) if config["use_pikpak"] else "0"
+        self.tg_api_id = str(config["tg_api_id"]) if config["tg_api_id"] else ""
+        self.tg_api_hash = str(config["tg_api_hash"]) if config["tg_api_hash"] else ""
+        self.use_cache = str(config["use_cache"]) if config["use_cache"] else "0"
+        self.redis_host = str(config["redis_host"]) if config["redis_host"] else ""
+        self.redis_port = str(config["redis_port"]) if config["redis_port"] else ""
+        self.redis_password = (
+            str(config["redis_password"]) if config["redis_password"] else ""
+        )
+        self.enable_nsfw = str(config["enable_nsfw"]) if config["enable_nsfw"] else "0"
+        # set
+        self.proxy_addr_dmm = ""
+        self.proxy_json = {"http": "", "https": ""}
+        self.proxy_json_pikpak = {}
+        if self.use_proxy == "1":
+            self.proxy_json = {"http": self.proxy_addr, "https": self.proxy_addr}
+            self.proxy_addr_dmm = self.proxy_addr
+            t1 = self.proxy_addr.find(":")
+            t2 = self.proxy_addr.rfind(":")
+            self.proxy_json_pikpak = {
+                "scheme": self.proxy_addr[:t1],
+                "hostname": self.proxy_addr[t1 + 3 : t2],
+                "port": int(self.proxy_addr[t2 + 1 :]),
+            }
+            LOG.info(f'Set proxy: "{self.proxy_addr}"')
+        elif self.use_proxy_dmm == "1":
+            self.proxy_addr_dmm = self.proxy_addr
+            self.proxy_addr = ""
+            LOG.info(f'Set dmm proxy: "{self.proxy_addr_dmm}"')
+        else:
+            self.proxy_addr = ""
+        LOG.info("Successfully read and loaded the configuration file.")
+
+
 
 BASE_URL_TG = "https://t.me"
 BASE_URL_MISS_AV = "https://missav.com"
